c5e62113c08de30ce5eb35cbe428a7b790ac2060
vsahasrabudhe96 <vsahasrabudhe96@gmail.com>
2022-08-09 02:22:26 -0400

    testing data and target split in main file

--- src/main.py
+++ src/main.py
@@ -3,16 +3,9 @@ from data_clean import DataSet
 if __name__ == "__main__":
     data = DataSet()
     data.load_csv()
-    
-    print("----Before Encoding the categorical values-----")
-    data.print_df()
-    
     int_col,str_col = data.continuous_categorical()
-    
     data.encoding()
-    
-    print("-----After encoding the categorical values-----")
-    
-    data.print_df()
+    X,y = data.data_target()
+
     
     
